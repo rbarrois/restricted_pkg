1f5810ef5e4147eed7828d791ac788f058379a1d
Benoit Calvez <benoit.calvez@polyconseil.fr>
2013-05-29 16:16:43 +0200

    [Py3] Import unicode_literals instead having trailing 'u'

--- restricted_pkg/__init__.py
+++ restricted_pkg/__init__.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 # Copyright (c) 2012-2013 Raphaël Barrois
 # License: MIT
+from __future__ import unicode_literals
 
-__author__ = u"Raphaël Barrois <raphael.barrois@polytechnique.org>"
+__author__ = "Raphaël Barrois <raphael.barrois@polytechnique.org>"
 __version__ = '0.1.0'
 
 from .commands import setup
